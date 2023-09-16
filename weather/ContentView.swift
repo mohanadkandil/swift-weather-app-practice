6d6f5471fcb5bb7f464c8be2f87a794345080ab2
mkandil <mkandil@synapse-analytics.io>
2023-09-16 12:25:37 +0300

    refactor: repalce all deprecated functions to the iOS 16 updates

--- weather/ContentView.swift
+++ weather/ContentView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
+            BackgroundView(isNight: isNight)
             VStack {
                 CityTextView(cityName: "Alexandria, Egypt")
                 
-                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 76)
                 
                 HStack(spacing: 20) {
                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 74)
@@ -32,9 +35,9 @@ struct ContentView: View {
                 Spacer()
                 
                 Button {
-                    print("Tapped")
+                    isNight.toggle()
                 } label: {
-                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                    WeatherButton(title: "Change Day Time")
                 }
                 
                 Spacer()
@@ -74,12 +77,12 @@ struct WeatherDayView: View {
 
 struct BackgroundView: View {
     
-    var topColor: Color
-    var bottomColor: Color
+    var isNight: Bool
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(.all)
+        ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea()
     }
 }
 
@@ -103,7 +106,7 @@ struct MainWeatherStatusView: View {
     var body: some View {
         VStack(spacing: 10) {
             Image(systemName: imageName)
-                .renderingMode(.original)
+                .symbolRenderingMode(.multicolor)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 180, height: 180)
@@ -119,14 +122,12 @@ struct MainWeatherStatusView: View {
 struct WeatherButton: View {
     
     var title: String
-    var textColor: Color
-    var backgroundColor: Color
-    
+
     var body: some View {
         Text(title)
             .frame(width: 280, height: 50)
-            .background(backgroundColor)
-            .foregroundColor(textColor)
+            .background(.mint)
+            .foregroundColor(.white)
             .font(.system(size: 20, weight: .bold, design: .default))
             .cornerRadius(10)
     }
