9a10babc69708efbabd36f28c3eab4e54f78fe46
mkandil <mkandil@synapse-analytics.io>
2023-09-16 10:10:44 +0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Mohanned Kandil on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
